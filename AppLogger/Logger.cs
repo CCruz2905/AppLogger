3aafb8274f29e89b7fe2884b0009b2924706ee90
Carlos Cruz <=>
2022-09-17 17:57:20 -0500

    Add project files.

---
+++ AppLogger/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace AppLogger
+{
+    public class Logger
+    {
+        private readonly string _path;
+        private readonly string AppName = Assembly.GetEntryAssembly().GetName().Name;
+
+        /// <summary>
+        /// Creates an object where the path is by default on My Documents
+        /// </summary>
+        public Logger()
+        {
+            _path = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+                AppName,
+                $"{AppName}Log.txt");
+        }
+
+        /// <summary>
+        /// Creates an object with path and file specified
+        /// </summary>
+        /// <param name="path">Relative path where log is going to be</param>
+        public Logger(string path)
+        {
+            _path = path;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void CreateFile()
+        {
+
+        }
+    }
+}
